da28ab91bf19b0e9d73c77c6b8cebf140992cbf6
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-02-23 18:20:26 -0800

    sidebar and wheel components initiated

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './Sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/components/wheel/Wheel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './Wheel.scss'
+
+const Wheel = () => {
+  return (
+    <div className='wheel'>
+      <h1 className='wheel-title'>WHEEL OF RR PARTNERS</h1>
+    </div>
+  )
+}
+
+export default Wheel
