9f98e889fa29420814d1b12eadbfa2ad26147ab4
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-02-27 14:14:30 -0800

    chart js installed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+import 'chart.js/auto';
+import { Data } from "./Data/Data";
+import PieChart from "./components/PieChart";
 import './App.scss';
+
+
 import Sidebar from './components/sidebar/Sidebar';
 import Wheel from './components/wheel/Wheel';
 
-function App() {
+export default function App() {
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+ 
   return (
     <div className="App">
-      <Sidebar />
-      <Wheel />
+      <p>Using Chart.js in React</p>
+      <PieChart chartData={chartData} />
     </div>
   );
 }
-
-export default App;
