e6dd39cea0e9ec2a64b6c38776f3c67eee74efa7
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-02-27 17:11:26 -0800

    chartjs removed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-import PieChart from "./components/PieChart";
-import {Chart} from 'chart.js';
-import ChartDataLabels from "chartjs-plugin-datalabels";
-import 'chart.js/auto';
 import { useState } from 'react';
-import { Data } from "./Data/Data";
 import './App.scss';
-import Sidebar from './components/sidebar/Sidebar';
-import Wheel from './components/wheel/Wheel';
-Chart.register(ChartDataLabels);
+import Sidebar from './components/sidebar/Sidebar'
+import Wheel from './components/wheel/Wheel'
+
 
 export default function App() {
-  const [chartData, setChartData] = useState({
-    labels: Data.map((data) => data.year), 
-    datasets: [
-      {
-        label: "Users Gained ",
-        data: Data.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0"
-        ],
-        borderColor: "black",
-        borderWidth: 2
-      }
-    ]
-  });
 
   return (
     <div className="App">
-      <p>Using Chart.js in React</p>
-      <PieChart chartData={chartData} />
+      {/* <Sidebar /> */}
+      <Wheel />
     </div>
   );
 }
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,31 +1,45 @@
 export const Data = [
   {
     id: 1,
+    slice: 1,
     name: 'Brandon',
+    group: 'dev-team'
+  },
+  {
+    id: 1,
+    slice: 1,
+    name: 'Jasira',
+    group: 'dev-team'
+  },
+  {
+    id: 1,
+    slice: 1,
+    name: 'Akili',
     group: 'dev-team'
   },
   {
     id: 2,
     year: 2017,
-    userGain: 45677,
+    userGain: 3,
     userLost: 345
   },
   {
     id: 3,
     year: 2018,
-    userGain: 78888,
-    userLost: 555
+    userGain: 1,
+    userLost: 555,
+    dataLabel: 'Brandon'
   },
   {
     id: 4,
     year: 2019,
-    userGain: 90000,
+    userGain: 1,
     userLost: 4555
   },
   {
     id: 5,
     year: 2020,
-    userGain: 4300,
+    userGain: 1,
     userLost: 234
   }
 ];
