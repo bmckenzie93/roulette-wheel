6ee22addba990c19009bcd62c922a521c581af2b
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-02-28 15:19:01 -0800

    lifted luchos html and css from the DOM, converted it to display as react component

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,45 +1,9 @@
-export const Data = [
-  {
-    id: 1,
-    slice: 1,
-    name: 'Brandon',
-    group: 'dev-team'
-  },
-  {
-    id: 1,
-    slice: 1,
-    name: 'Jasira',
-    group: 'dev-team'
-  },
-  {
-    id: 1,
-    slice: 1,
-    name: 'Akili',
-    group: 'dev-team'
-  },
-  {
-    id: 2,
-    year: 2017,
-    userGain: 3,
-    userLost: 345
-  },
-  {
-    id: 3,
-    year: 2018,
-    userGain: 1,
-    userLost: 555,
-    dataLabel: 'Brandon'
-  },
-  {
-    id: 4,
-    year: 2019,
-    userGain: 1,
-    userLost: 4555
-  },
-  {
-    id: 5,
-    year: 2020,
-    userGain: 1,
-    userLost: 234
-  }
+const Data = [
+  'Brandon',
+  'Jasira',
+  'Akili',
+  'Grandpa',
+  'Dan',
+  'Jeremy'
 ];
+export default Data;
--- src/components/wheel/Wheel.js
+++ src/components/wheel/Wheel.js
@@ -1,12 +1,59 @@
-import React from 'react'
-import Styles from './Wheel.scss'
+import React, { useState } from "react";
+
+import Styles from "./Wheel.scss"; 
+import Data from "../../Data/Data.js"
 
 const Wheel = () => {
-  return (
-    <div className='wheel'>
-      <h1 className='wheel-title'>WHEEL OF RR PARTNERS</h1>
-    </div>
-  )
-}
+  // STATES
+  const [nameArray, setNameArray] = useState(Data)
+  const [selectedNumber, setSelectedNumber] = useState(0)
 
-export default Wheel
+  // EVENTS
+  const handleSelectNumber = () => {
+    const winner = Math.floor(Math.random() * (( nameArray.length + 1 ) - 0) + 0)
+    setSelectedNumber(winner)
+  }
+
+  // MAPS
+  const wheelItems = nameArray.map( (name, index) => {
+    const wheelPlacement = { 
+      transform: `translateY(-50%) rotate(calc(${index}*360deg/${nameArray.length}))`
+    }
+
+    return (
+    <div key={index} 
+        className="wheel-item animated fadeIn selected"
+        style={wheelPlacement}>
+      {name}
+    </div>
+  )})
+
+  // MAIN
+  return (
+    <main>
+      <div className="container main-container">
+        <h3 className="notice">Click the wheel to spin</h3>
+        <div className="wheel-container">
+
+          <div className="wheel animated fadeIn spinning" onClick={handleSelectNumber}>
+            {wheelItems}
+          </div>
+        </div>
+
+        <div className="person-selected">
+          <div className="person-selected__name">
+            <button
+              className=" "
+              tabIndex="0"
+              type="button">
+              <span>SELECT 'SELECTED PERSON'</span>
+              <span>{selectedNumber}</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Wheel;
